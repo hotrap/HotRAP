9f0c4416fe9229da9d82629b267b222bd68db0ab
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-04-07 19:40:28 +0800

    clang-format compaction_router.h

--- include/rocksdb/compaction_router.h
+++ include/rocksdb/compaction_router.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "rocksdb/customizable.h"
-#include "rocksdb/rocksdb_namespace.h"
-
 #include <algorithm>
 #include <chrono>
 #include <ostream>
+
+#include "rocksdb/customizable.h"
+#include "rocksdb/rocksdb_namespace.h"
 
 namespace ROCKSDB_NAMESPACE {
 
@@ -22,18 +22,18 @@ enum class TimerType : size_t {
 };
 constexpr size_t timer_num = static_cast<size_t>(TimerType::kEnd);
 
-extern const char* timer_names[];
+extern const char *timer_names[];
 
 enum class PerLevelTimerType {
   kPickSST,
   kEnd,
 };
 constexpr size_t per_level_timer_num =
-  static_cast<size_t>(PerLevelTimerType::kEnd);
+    static_cast<size_t>(PerLevelTimerType::kEnd);
 extern const char *per_level_timer_names[];
 
 struct TimerStatus {
-  const char* name;
+  const char *name;
   uint64_t count;
   uint64_t nsec;
 };
@@ -48,34 +48,41 @@ class CompactionRouter : public Customizable {
   CompactionRouter() : num_used_levels_(0) {}
   virtual ~CompactionRouter() {}
   static const char *Type() { return "CompactionRouter"; }
-  static Status CreateFromString(const ConfigOptions &config_options, const std::string &name, const CompactionRouter **result);
+  static Status CreateFromString(const ConfigOptions &config_options,
+                                 const std::string &name,
+                                 const CompactionRouter **result);
   const char *Name() const override = 0;
   virtual size_t Tier(int level) = 0;
-  virtual void AddHotness(size_t tier, const rocksdb::Slice &key, size_t vlen, double weight) = 0;
+  virtual void AddHotness(size_t tier, const rocksdb::Slice &key, size_t vlen,
+                          double weight) = 0;
   virtual void Access(int level, const Slice &key, size_t vlen) = 0;
   virtual void *NewIter(size_t tier) = 0;
-  virtual const rocksdb::HotRecInfo *Seek(void *iter, const rocksdb::Slice &key) = 0;
+  virtual const rocksdb::HotRecInfo *Seek(void *iter,
+                                          const rocksdb::Slice &key) = 0;
   virtual const HotRecInfo *NextHot(void *iter) = 0;
   virtual void DelIter(void *iter) = 0;
-  virtual void DelRange(size_t tier, const rocksdb::Slice &smallest, const rocksdb::Slice &largest) = 0;
-  virtual size_t RangeHotSize(size_t tier, const rocksdb::Slice &smallest, const rocksdb::Slice &largest) = 0;
+  virtual void DelRange(size_t tier, const rocksdb::Slice &smallest,
+                        const rocksdb::Slice &largest) = 0;
+  virtual size_t RangeHotSize(size_t tier, const rocksdb::Slice &smallest,
+                              const rocksdb::Slice &largest) = 0;
 
-	static std::chrono::steady_clock::time_point Start() {
-	    return std::chrono::steady_clock::now();
-	}
-	void Stop(TimerType type, std::chrono::steady_clock::time_point start_time) {
-		auto end_time = std::chrono::steady_clock::now();
-		auto nsec = std::chrono::duration_cast<std::chrono::nanoseconds>(
-				end_time - start_time).count();
-		AddTimer(type, nsec);
-	}
+  static std::chrono::steady_clock::time_point Start() {
+    return std::chrono::steady_clock::now();
+  }
+  void Stop(TimerType type, std::chrono::steady_clock::time_point start_time) {
+    auto end_time = std::chrono::steady_clock::now();
+    auto nsec = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time -
+                                                                     start_time)
+                    .count();
+    AddTimer(type, nsec);
+  }
   void AddTimer(TimerType type, uint64_t nsec) {
     timers_[static_cast<size_t>(type)].add(nsec);
   }
   std::vector<TimerStatus> CollectTimers() {
     std::vector<TimerStatus> ret;
     for (size_t id = 0; id < timer_num; id += 1) {
-      const auto& timer = timers_[id];
+      const auto &timer = timers_[id];
       ret.push_back(TimerStatus{timer_names[id], timer.count(), timer.nsec()});
     }
     return ret;
@@ -86,9 +93,9 @@ class CompactionRouter : public Customizable {
     for (size_t level = 0; level < num_used_levels; ++level) {
       std::vector<TimerStatus> timers;
       for (size_t id = 0; id < per_level_timer_num; id += 1) {
-        const auto& timer = per_level_timers_[level][id];
+        const auto &timer = per_level_timers_[level][id];
         timers.push_back(TimerStatus{per_level_timer_names[id], timer.count(),
-            timer.nsec()});
+                                     timer.nsec()});
       }
       ret.push_back(std::move(timers));
     }
@@ -99,17 +106,19 @@ class CompactionRouter : public Customizable {
     int num_used_levels = num_used_levels_.load(std::memory_order_relaxed);
     while (num_used_levels <= level) {
       num_used_levels_.compare_exchange_weak(num_used_levels, level + 1,
-        std::memory_order_relaxed);
+                                             std::memory_order_relaxed);
     }
     per_level_timers_[level][static_cast<size_t>(type)].add(nsec);
   }
   void Stop(int level, PerLevelTimerType type,
-      std::chrono::steady_clock::time_point start_time) {
+            std::chrono::steady_clock::time_point start_time) {
     auto end_time = std::chrono::steady_clock::now();
-		auto nsec = std::chrono::duration_cast<std::chrono::nanoseconds>(
-				end_time - start_time).count();
-		AddTimerInLevel(level, type, nsec);
+    auto nsec = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time -
+                                                                     start_time)
+                    .count();
+    AddTimerInLevel(level, type, nsec);
   }
+
  private:
   class Timer {
    public:
@@ -120,6 +129,7 @@ class CompactionRouter : public Customizable {
       count_.fetch_add(1, std::memory_order_relaxed);
       nsec_.fetch_add(nsec, std::memory_order_relaxed);
     }
+
    private:
     std::atomic<uint64_t> count_;
     std::atomic<uint64_t> nsec_;
