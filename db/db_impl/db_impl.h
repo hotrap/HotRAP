8402836d7dd3b8c1c39d55618e0ae20474c50167
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-18 18:15:33 +0800

    CPU time of checker

--- db/db_impl/db_impl.h
+++ db/db_impl/db_impl.h
@@ -1392,9 +1392,7 @@ class DBImpl : public DB {
   friend class WriteUnpreparedTransactionTest_RecoveryTest_Test;
 #endif
 
-  friend void check_newer_version(DBImpl* db, SuperVersion* sv,
-                                  int target_level,
-                                  std::list<ImmPromotionCache>::iterator iter);
+  friend class PromotionCache;
 
   struct CompactionState;
   struct PrepickedCompaction;
