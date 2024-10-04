36ae1661c53fc12090915c0349d8f614ddb45132
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-10-04 11:42:09 +0800

    report_operation_count_time

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -1206,6 +1206,10 @@ DEFINE_string(report_file, "report.csv",
               "Filename where some simple stats are reported to (if "
               "--report_interval_seconds is bigger than 0)");
 
+DEFINE_bool(report_operation_count_time, false,
+            "If true, report the count and time of each operation type every "
+            "<report_interval_seconds> seconds in file <op-type>-count-time.");
+
 DEFINE_int32(thread_status_per_interval, 0,
              "Takes and report a snapshot of the current status of each thread"
              " when this is greater than 0.");
@@ -2045,6 +2049,28 @@ struct DBWithColumnFamilies {
   }
 };
 
+enum OperationType : unsigned char {
+  kRead = 0,
+  kWrite,
+  kDelete,
+  kSeek,
+  kMerge,
+  kUpdate,
+  kCompress,
+  kUncompress,
+  kCrc,
+  kHash,
+  kOthers
+};
+
+static std::unordered_map<OperationType, std::string, std::hash<unsigned char>>
+    OperationTypeString = {{kRead, "read"},         {kWrite, "write"},
+                           {kDelete, "delete"},     {kSeek, "seek"},
+                           {kMerge, "merge"},       {kUpdate, "update"},
+                           {kCompress, "compress"}, {kCompress, "uncompress"},
+                           {kCrc, "crc"},           {kHash, "hash"},
+                           {kOthers, "op"}};
+
 // a class that reports stats to CSV file
 class ReporterAgent {
  public:
@@ -2084,6 +2110,21 @@ class ReporterAgent {
   void ReportFinishedOps(int64_t num_ops) {
     total_ops_done_.fetch_add(num_ops);
   }
+  // thread safe
+  void FinishedOps(int64_t num_ops, enum OperationType op_type,
+                   uint64_t micros) {
+    ReportFinishedOps(num_ops);
+    auto it = op_count_time_.find(op_type);
+    if (it == op_count_time_.end()) {
+      auto res = op_count_time_.emplace(std::piecewise_construct,
+                                        std::forward_as_tuple(op_type),
+                                        std::forward_as_tuple());
+      assert(res.second);
+      it = res.first;
+    }
+    it->second.count.fetch_add(num_ops, std::memory_order_relaxed);
+    it->second.micros.fetch_add(micros, std::memory_order_relaxed);
+  }
 
  private:
   std::string Header() const { return "secs_elapsed,interval_qps"; }
@@ -2119,6 +2160,44 @@ class ReporterAgent {
                 s.ToString().c_str());
         break;
       }
+      for (const auto& op_count_time : op_count_time_) {
+        OperationType op = op_count_time.first;
+        auto it = op_report_file_.find(op);
+        if (it == op_report_file_.end()) {
+          std::string fname = OperationTypeString[op] + "-count-time";
+          std::unique_ptr<WritableFile> file;
+          s = env_->NewWritableFile(fname, &file, EnvOptions());
+          if (s.ok()) {
+            s = file->Append("secs_elapsed count micros\n");
+          }
+          if (s.ok()) {
+            s = file->Flush();
+          }
+          if (!s.ok()) {
+            fprintf(stderr, "Can't open %s: %s\n", fname.c_str(),
+                    s.ToString().c_str());
+            abort();
+          }
+          auto res = op_report_file_.emplace(op, std::move(file));
+          assert(res.second);
+          it = res.first;
+        }
+        report =
+            std::to_string(secs_elapsed) + ' ' +
+            std::to_string(
+                op_count_time.second.count.load(std::memory_order_relaxed)) +
+            ' ' +
+            std::to_string(
+                op_count_time.second.micros.load(std::memory_order_relaxed)) +
+            '\n';
+        s = it->second->Append(report);
+        if (!s.ok()) {
+          fprintf(stderr,
+                  "Can't write to report file (%s), stopping the reporting\n",
+                  s.ToString().c_str());
+          return;
+        }
+      }
       last_report_ = total_ops_done_snapshot;
     }
   }
@@ -2128,40 +2207,21 @@ class ReporterAgent {
   std::atomic<int64_t> total_ops_done_;
   int64_t last_report_;
   const uint64_t report_interval_secs_;
+
+  std::unordered_map<OperationType, std::unique_ptr<WritableFile>>
+      op_report_file_;
+  struct CountTime {
+    std::atomic<uint64_t> count;
+    std::atomic<uint64_t> micros;
+    CountTime() : count(0), micros(0) {}
+  };
+  std::unordered_map<OperationType, CountTime> op_count_time_;
+
   ROCKSDB_NAMESPACE::port::Thread reporting_thread_;
   std::mutex mutex_;
   // will notify on stop
   std::condition_variable stop_cv_;
   bool stop_;
-};
-
-enum OperationType : unsigned char {
-  kRead = 0,
-  kWrite,
-  kDelete,
-  kSeek,
-  kMerge,
-  kUpdate,
-  kCompress,
-  kUncompress,
-  kCrc,
-  kHash,
-  kOthers
-};
-
-static std::unordered_map<OperationType, std::string, std::hash<unsigned char>>
-                          OperationTypeString = {
-  {kRead, "read"},
-  {kWrite, "write"},
-  {kDelete, "delete"},
-  {kSeek, "seek"},
-  {kMerge, "merge"},
-  {kUpdate, "update"},
-  {kCompress, "compress"},
-  {kCompress, "uncompress"},
-  {kCrc, "crc"},
-  {kHash, "hash"},
-  {kOthers, "op"}
 };
 
 class CombinedStats;
@@ -2293,25 +2353,31 @@ class Stats {
 
   void FinishedOps(DBWithColumnFamilies* db_with_cfh, DB* db, int64_t num_ops,
                    enum OperationType op_type = kOthers) {
-    if (reporter_agent_) {
-      reporter_agent_->ReportFinishedOps(num_ops);
-    }
-    if (FLAGS_histogram) {
+    if (FLAGS_histogram || FLAGS_report_operation_count_time) {
       uint64_t now = clock_->NowMicros();
       uint64_t micros = now - last_op_finish_;
 
-      if (hist_.find(op_type) == hist_.end())
-      {
-        auto hist_temp = std::make_shared<HistogramImpl>();
-        hist_.insert({op_type, std::move(hist_temp)});
-      }
-      hist_[op_type]->Add(micros);
+      if (FLAGS_histogram) {
+        if (hist_.find(op_type) == hist_.end()) {
+          auto hist_temp = std::make_shared<HistogramImpl>();
+          hist_.insert({op_type, std::move(hist_temp)});
+        }
+        hist_[op_type]->Add(micros);
 
-      if (micros > 20000 && !FLAGS_stats_interval) {
-        fprintf(stderr, "long op: %" PRIu64 " micros%30s\r", micros, "");
-        fflush(stderr);
+        if (micros > 20000 && !FLAGS_stats_interval) {
+          fprintf(stderr, "long op: %" PRIu64 " micros%30s\r", micros, "");
+          fflush(stderr);
+        }
+      }
+      if (FLAGS_report_operation_count_time) {
+        assert(reporter_agent_);
+        reporter_agent_->FinishedOps(num_ops, op_type, micros);
+      } else if (reporter_agent_) {
+        reporter_agent_->ReportFinishedOps(num_ops);
       }
       last_op_finish_ = now;
+    } else if (reporter_agent_) {
+      reporter_agent_->ReportFinishedOps(num_ops);
     }
 
     done_ += num_ops;
@@ -6294,6 +6360,11 @@ class Benchmark {
         // the Get query
         gets++;
         read++;
+        if (thread->shared->read_rate_limiter && read % 100 == 0) {
+          thread->shared->read_rate_limiter->Request(100, Env::IO_HIGH,
+                                                     nullptr /*stats*/);
+          thread->stats.ResetLastOpTime();
+        }
         if (FLAGS_num_column_families > 1) {
           s = db_with_cfh->db->Get(options, db_with_cfh->GetCfh(key_rand), key,
                                    &pinnable_val);
@@ -6312,14 +6383,15 @@ class Benchmark {
           abort();
         }
 
-        if (thread->shared->read_rate_limiter && read % 100 == 0) {
-          thread->shared->read_rate_limiter->Request(100, Env::IO_HIGH,
-                                                     nullptr /*stats*/);
-        }
         thread->stats.FinishedOps(db_with_cfh, db_with_cfh->db, 1, kRead);
       } else if (query_type == 1) {
         // the Put query
         puts++;
+        if (thread->shared->write_rate_limiter && puts % 32 == 0) {
+          thread->shared->write_rate_limiter->Request(32, Env::IO_HIGH,
+                                                      nullptr /*stats*/);
+          thread->stats.ResetLastOpTime();
+        }
         int64_t val_size = ParetoCdfInversion(
             u, FLAGS_value_theta, FLAGS_value_k, FLAGS_value_sigma);
         if (val_size < 0) {
@@ -6335,10 +6407,6 @@ class Benchmark {
           ErrorExit();
         }
 
-        if (thread->shared->write_rate_limiter && puts % 100 == 0) {
-          thread->shared->write_rate_limiter->Request(100, Env::IO_HIGH,
-                                                      nullptr /*stats*/);
-        }
         thread->stats.FinishedOps(db_with_cfh, db_with_cfh->db, 1, kWrite);
       } else if (query_type == 2) {
         // Seek query
