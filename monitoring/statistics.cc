32381b6e759cdefd2a2258f44577747e37cc9adc
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-10-27 06:56:49 +0000

    Add names for new tickers

--- monitoring/statistics.cc
+++ monitoring/statistics.cc
@@ -224,6 +224,8 @@ const std::vector<std::pair<Tickers, std::string>> TickersNameMap = {
     {HOT_FILE_READ_COUNT, "rocksdb.hot.file.read.count"},
     {WARM_FILE_READ_COUNT, "rocksdb.warm.file.read.count"},
     {COLD_FILE_READ_COUNT, "rocksdb.cold.file.read.count"},
+    {LEADER_WRITE_COUNT, "rocksdb.leader.write.count"},
+    {NON_LEADER_WRITE_COUNT, "rocksdb.nonleader.write.count"},
 };
 
 const std::vector<std::pair<Histograms, std::string>> HistogramsNameMap = {
